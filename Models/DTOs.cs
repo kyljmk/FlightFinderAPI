c65504472e7cc395e5642bfa59349e016cb00924
kyljmk <k.mansonkullin@googlemail.com>
2022-12-13 16:26:39 +0100

    Add DTOs

---
+++ Models/DTOs.cs
@@ -0,0 +1,12 @@
+namespace FlightFinderAPI.Models;
+
+public class SearchRequest
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? Option { get; set; }
+    public string? Departure { get; set; }
+    public string? Arrival { get; set; }
+    public int Adults { get; set; }
+    public int Children { get; set; }
+}
